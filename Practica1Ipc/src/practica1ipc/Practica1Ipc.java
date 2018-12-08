97237f81b07a910c9c60b3cd001ab3a00b3d565a
DANIEL <DANIEL@CASA>
2018-12-08 12:39:40 -0600

    Primeras líneas

--- Practica1Ipc/src/practica1ipc/Practica1Ipc.java
+++ Practica1Ipc/src/practica1ipc/Practica1Ipc.java
@@ -1,21 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package practica1ipc;
+
+import java.util.Scanner;
+import java.util.Random;
 
 /**
  *
  * @author DANIEL
  */
 public class Practica1Ipc {
+private String nombreUsuario;
+    private String password;
+    Scanner s= new Scanner(System.in);//Variable que leera las entradas
+    
+    
+    public void entradaSistema(){
+        System.out.println("Bienvenido");
+        System.out.println("");
+        
+        System.out.println("Ingrese su nobre de usuarrio: ");
+        nombreUsuario=s.nextLine();
+        
+        System.out.println("Ingrese su contraseña: ");
+        password=s.nextLine();
+        
+        
+        if(nombreUsuario.equalsIgnoreCase("Vendedor"));
+        
+        
+        
+    }
+    
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+           Practica1Ipc objeto=new Practica1Ipc();
+        objeto.entradaSistema();
+        
+     
     }
     
 }
